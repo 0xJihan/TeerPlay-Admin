a33f94ed9f44391623b2def3e0d4424c2c151c7e
0xJihan <jihankhan966@gmail.com>
2025-05-15 12:00:28 +0600

    refactor: update AppInfo model
    - Replaced `telegramLink` with `whatsAppLink`.
    - Added fields for `fbLink`, `fbGroupLink`, `appVersion`, `appLink`, `changeLog`, `forceAppUpdate`, `withdrawVideo`, and `playNowVideo`.

--- app/src/main/java/com/jihan/teeradmin/data/models/AppInfo.kt
+++ app/src/main/java/com/jihan/teeradmin/data/models/AppInfo.kt
@@ -5,11 +5,22 @@ data class AppInfo(
     val bkashNumber: String="N/A",
     val nagadNumber: String="N/A",
     val rocketNumber: String="N/A",
-    val telegramLink: String="N/A",
+    val whatsAppLink: String="N/A",
 
     val googlePayNumber: String="N/A",
     val phonePeNumber: String="N/A",
     val paytmNumber: String="N/A",
 
+
+    val fbLink: String="N/A",
+    val fbGroupLink: String="N/A",
+
+    val appVersion: String="",
+    val appLink: String="",
+    val changeLog: String="",
+    val forceAppUpdate: Boolean=true,
+
+    val withdrawVideo: String="",
+    val playNowVideo: String="",
 
 )
